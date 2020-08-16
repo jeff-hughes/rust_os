46e05938ca748591e60b6971e5239abe78cb9f26
Jeff Hughes <jeff.hughes@gmail.com>
2020-08-16 13:32:41 -0400

    Send exit code to QEMU when running tests

--- src/main.rs
+++ src/main.rs
@@ -37,6 +37,28 @@ fn test_runner(tests: &[&dyn Fn()]) {
     for test in tests {
         test();
     }
+    exit_qemu(QemuExitCode::Success);
+}
+
+/// Enum specifying the exit code when running tests.
+#[derive(Debug, Clone, Copy, PartialEq, Eq)]
+#[repr(u32)]
+pub enum QemuExitCode {
+    Success = 0x10,
+    Failed = 0x11,
+}
+
+/// Write to a specified port to exit QEMU with the
+/// given exit code. The 0xf4 port is specified in
+/// Cargo.toml in the metadata given to the bootloader
+/// crate.
+pub fn exit_qemu(exit_code: QemuExitCode) {
+    use x86_64::instructions::port::Port;
+
+    unsafe {
+        let mut port = Port::new(0xf4);
+        port.write(exit_code as u32);
+    }
 }
 
 
