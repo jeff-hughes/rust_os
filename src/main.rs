0060f93bb30d22dc62f8d1e4a36bdeab6f01be56
Jeff Hughes <jeff.hughes@gmail.com>
2020-08-16 13:56:35 -0400

    Add trait for flavour text for test functions

--- src/main.rs
+++ src/main.rs
@@ -44,12 +44,27 @@ fn panic(info: &PanicInfo) -> ! {
 
 /// Custom test runner for running unit and integration tests.
 #[cfg(test)]
-fn test_runner(tests: &[&dyn Fn()]) {
+fn test_runner(tests: &[&dyn Testable]) {
     serial_println!("Running {} tests", tests.len());
     for test in tests {
-        test();
+        test.run();
     }
     exit_qemu(QemuExitCode::Success);
+}
+
+/// Trait for test functions.
+pub trait Testable {
+    fn run(&self) -> ();
+}
+
+impl<T> Testable for T where T: Fn() {
+    /// Adds some flavour text around tests that prints the
+    /// function's name and an [ok] on success.
+    fn run(&self) {
+        serial_print!("{}...\t", core::any::type_name::<T>());
+        self();
+        serial_println!("[ok]");
+    }
 }
 
 /// Enum specifying the exit code when running tests.
@@ -76,7 +91,5 @@ pub fn exit_qemu(exit_code: QemuExitCode) {
 
 #[test_case]
 fn trivial_assertion() {
-    serial_print!("trivial assertion... ");
-    assert_eq!(0, 1);
-    serial_println!("[ok]");
+    assert_eq!(1, 1);
 }
