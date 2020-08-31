31698fc3e81c50ef3795c34c1a40c159ba887498
Jeff Hughes <jeff.hughes@gmail.com>
2020-08-31 15:48:25 -0400

    Add simple bump allocator for heap

--- src/allocator.rs
+++ src/allocator.rs
@@ -1,4 +1,4 @@
-use linked_list_allocator::LockedHeap;
+// use linked_list_allocator::LockedHeap;
 use x86_64::{
     structures::paging::{
         mapper::MapToError, FrameAllocator, Mapper, Page, PageTableFlags, Size4KiB,
@@ -6,11 +6,16 @@ use x86_64::{
     VirtAddr,
 };
 
+pub mod bump;
+
+use bump::BumpAllocator;
+
 pub const HEAP_START: usize = 0x_4444_4444_0000;
 pub const HEAP_SIZE: usize = 100 * 1024; // 100 KiB
 
 #[global_allocator]
-static ALLOCATOR: LockedHeap = LockedHeap::empty();
+static ALLOCATOR: Locked<BumpAllocator> = Locked::new(BumpAllocator::new());
+// static ALLOCATOR: LockedHeap = LockedHeap::empty();
 
 
 /// Initialize a heap region of memory of `HEAP_SIZE` size, given a memory
@@ -44,4 +49,30 @@ pub fn init_heap(
     }
 
     Ok(())
+}
+
+
+/// Align the given address `addr` upwards to alignment `align`.
+/// See here for explanation:
+/// https://os.phil-opp.com/allocator-designs/#address-alignment
+fn align_up(addr: usize, align: usize) -> usize {
+    (addr + align - 1) & !(align - 1)
+}
+
+
+/// A wrapper around spin::Mutex to permit trait implementations.
+pub struct Locked<A> {
+    inner: spin::Mutex<A>,
+}
+
+impl<A> Locked<A> {
+    pub const fn new(inner: A) -> Self {
+        Locked {
+            inner: spin::Mutex::new(inner),
+        }
+    }
+
+    pub fn lock(&self) -> spin::MutexGuard<A> {
+        self.inner.lock()
+    }
 }
